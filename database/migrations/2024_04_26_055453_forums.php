7bc5b901faf55e2a5a28f4ee910cf8b78677ba97
Hkaar <HkaarDev@gmail.com>
2024-04-28 12:36:20 +0800

    Updated forum migrations

--- database/migrations/2024_04_26_055453_forums.php
+++ database/migrations/2024_04_26_055453_forums.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 100);
             $table->text('desc')->nullable();
+            $table->timestamps();
         });
     }
 
