342982706e94f17fd111c01f89ffd1e6b53fa64a
Hkaar <HkaarDev@gmail.com>
2024-04-28 12:37:18 +0800

    Updated Follow Model

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'following';
 }
