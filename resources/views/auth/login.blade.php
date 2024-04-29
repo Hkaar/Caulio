f6a9fc3a2c0a7563b4919245388cc8255ca4be79
Hkaar <HkaarDev@gmail.com>
2024-04-29 14:57:29 +0800

    Added login & register views

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', "Login")
+
+@section('content')
+<section class="container">
+  <div class="flex flex-col justify-center content-center md:border md:border-primary md:rounded-lg">
+    Hello
+  </div>
+</section>
+@endsection
