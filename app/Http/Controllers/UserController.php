4cc74c1d9639d2bdbccab22359c57b9d9bf110fc
Hkaar <HkaarDev@gmail.com>
2024-05-01 23:11:08 +0800

    Updated user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -11,7 +15,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::paginate(20);
+
+        return view('users.index', [
+            "users" => $users,
+        ]);
     }
 
     /**
@@ -19,7 +27,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('users.create');
     }
 
     /**
@@ -27,38 +35,117 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "name" => ["required", "string", "max:255", "unique:users,name"],
+            "display_name" => ["nullable", "string", "max:255"],
+            "email" => ["required", "email", "unique:users,email"],
+            "password" => ["required", "string", "min:8", "confirm"],
+            "password_confirmation" => ["required", "string"],
+            "level" => ["nullable", "string"],
+            "img" => ["nullable", "img", "mimes:jpeg,png,jpg", "max:10240"]
+        ]);
+
+        $user = new User();
+        $user->fill($validated);
+
+        if ($request->hasFile('img')) {
+            $img = $request->file("img");
+
+            $fileName = time() . '_' . $img->getClientOriginalName();
+            $filePath = $img->storeAs('uploads', $fileName, 'public');
+
+            $user->img = $filePath;
+        }
+        
+        $user->password = Hash::make($validated["password"]);
+        $user->save();
+
+        return redirect()->route("users.index");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return view("users.show", [
+            "user" => $user,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(int $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return view("users.edit", [
+            "user" => $user,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            "name" => ["nullable", "string", "max:255", "unique:users,name"],
+            "display_name" => ["nullable", "string", "max:255"],
+            "email" => ["nullable", "email", "unique:users,email"],
+            "password" => ["nullable", "string", "min:8", "confirm"],
+            "password_confirmation" => ["nullable", "string"],
+            "level" => ["nullable", "string"],
+            "img" => ["nullable", "img", "mimes:jpeg,png,jpg", "max:10240"]
+        ]);
+
+        $user->update($validated);
+
+        if ($request->hasFile('img')) {
+            $img = $request->file("img");
+
+            if ($user->img) {
+                Storage::disk("public")->delete($user->img);
+            }    
+
+            $fileName = time() . '_' . $img->getClientOriginalName();
+            $filePath = $img->storeAs('uploads', $fileName, 'public');
+
+            $user->img = $filePath;
+        }
+        
+        if ($validated["password"]) {
+            $user->password = Hash::make($validated["password"]);
+        }
+
+        $user->save();
+
+        return redirect()->route("users.index");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user->forums()->delete();
+        $user->posts()->delete();
+        $user->comments()->delete();
+        $user->replies()->delete();
+
+        if ($user->img) {
+            Storage::disk("public")->delete($user->img);
+        }
+
+        $user->delete();
+
+        return response(null);
     }
 }
