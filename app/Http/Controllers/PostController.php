54bcf7b8beeccdd2645aa12956d2f33aec6cb5ab
Hkaar <HkaarDev@gmail.com>
2024-05-01 23:11:00 +0800

    Updated post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Forum;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,7 +15,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::paginate(20);
+
+        return view("posts.index", [
+            "posts" => $posts,
+        ]);
     }
 
     /**
@@ -19,7 +27,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view("posts.create");
     }
 
     /**
@@ -27,38 +35,90 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "forum" => ["required", "string", "max:100", "exists:forms,title"],
+            "user" => ["required", "string", "exists:users,name"],
+            "title" => ["required", "string", "max:255"],
+            "content" => ["nullable", "string"],
+        ]);
+
+        $post = new Post();
+
+        $userId = User::where("name", $validated["user"])->first()->id;
+        $forumId = Forum::where("title", $validated["forum"])->first()->id;
+
+        $post->fill($validated);
+        $post->user_id = $userId;
+        $post->forum_id = $forumId;
+
+        $post->save();
+
+        return redirect()->route("posts.index");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        
+        return view("posts.show", [
+            "post" => $post,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(int $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        
+        return view("posts.edit", [
+            "post" => $post,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        
+        $validated = $request->validate([
+            "forum" => ["nullable", "string", "max:100", "exists:forms,title"],
+            "user" => ["nullable", "string", "exists:users,name"],
+            "title" => ["nullable", "string", "max:255"],
+            "content" => ["nullable", "string"],
+        ]);
+
+        $post->update($validated);
+
+        if ($validated["forum"]) {
+            $userId = User::where("name", $validated["user"])->first()->id;
+            $post->user_id = $userId;
+        }
+
+        if ($validated["user"]) {
+            $forumId = Forum::where("title", $validated["forum"])->first()->id;
+            $post->forum_id = $forumId;
+        }
+
+        $post->save();
+
+        return redirect()->route("posts.index");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return response(null);
     }
 }
