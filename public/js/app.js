bb04185d201bc93fdab7e6e14cb7857dd86d6f52
Hkaar <HkaarDev@gmail.com>
2024-05-03 23:22:13 +0800

    Updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,22 @@
  */
 function toggleSideBar() {
     const sideBar = document.getElementById("sideBar");
+    const sideBarMenus = document.querySelectorAll(".menu-text");
 
-    if (sideBar?.classList.contains("-translate-x-full")) {
+    if (sideBar?.classList.contains("-translate-x-full") || sideBar?.classList.contains("md:max-w-20")) {
         sideBar.classList.replace("-translate-x-full", "translate-x-0");
+        sideBar.classList.replace("md:max-w-20", "md:max-w-48");
+
+        sideBarMenus.forEach((e) => {
+            e.classList.remove("hidden");
+        })
     } else {
         sideBar?.classList.replace("translate-x-0", "-translate-x-full");
+        sideBar?.classList.replace("md:max-w-48", "md:max-w-20");
+
+        sideBarMenus.forEach((e) => {
+            e.classList.add("hidden");
+        })
     }
 }
 
