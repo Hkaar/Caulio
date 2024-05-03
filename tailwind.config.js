58be805bad0d6b0dfec9a90a5ce677c8bdd3e699
Hkaar <HkaarDev@gmail.com>
2024-05-03 23:20:01 +0800

    Added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "2rem",
-      },
-      colors: {
-        "primary": "#1B1A55",
-        "secondary": "#535C91",
-        "accent": "#9290C3",
-        "error": "#E61B37",
-        "warning": "#FFA500",
-        "success": "#267D48",
-        "info": "#1FA2E2"        
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"]
-      }
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: "2rem",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            "light",
+            "dark",
+            "cupcake",
+            "forest",
+        ],
+    },
+};
