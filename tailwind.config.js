02dad91769613b2201951ccd1de1256ddb2d78d4
Hkaar <HkaarDev@gmail.com>
2024-05-06 12:42:04 +0800

    Updated tailwind config & tsconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "business",
     ],
-    theme: {
-        extend: {
-            container: {
-                center: true,
-                padding: "2rem",
-            },
-            fontFamily: {
-                poppins: ["Poppins", "sans-serif"],
-            },
-        },
-    },
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: [
-            "light",
-            "dark",
-            "cupcake",
-            "business",
-        ],
-    },
+  },
 };
