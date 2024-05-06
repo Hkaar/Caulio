d69152e386a9a53ddd7a4a9d97b9b8c1875a72cd
Hkaar <HkaarDev@gmail.com>
2024-05-06 12:44:51 +0800

    Updated user dashboard

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard - Users')
+
+@section('content')
+<section class="flex">
+  <x-dashboard-side-bar></x-dashboard-side-bar>
+  
+  <div class="flex flex-col flex-1">
+    <x-dashboard-navigation></x-dashboard-navigation>
+
+    <div class="flex flex-col overflow-x-auto px-6 py-4 h-full">
+      
+    </div>
+  </div>
+</section>
+@endsection
