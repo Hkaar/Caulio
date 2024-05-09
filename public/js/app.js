42e11e298e7ea3c6b03d4f90d95bddc2f6a5d16a
Hkaar <HkaarDev@gmail.com>
2024-05-09 22:50:08 +0800

    Fixed dashboard side bar expand bug

--- public/js/app.js
+++ public/js/app.js
@@ -5,16 +5,16 @@ function toggleSideBar() {
     const sideBar = document.getElementById("sideBar");
     const sideBarMenus = document.querySelectorAll(".menu-text");
 
-    if (sideBar?.classList.contains("-translate-x-full") || sideBar?.classList.contains("md:max-w-20")) {
+    if (sideBar?.classList.contains("-translate-x-full") && sideBar?.classList.contains("min-w-16")) {
         sideBar.classList.replace("-translate-x-full", "translate-x-0");
-        sideBar.classList.replace("md:max-w-20", "md:max-w-48");
+        sideBar.classList.replace("min-w-16", "min-w-80");
 
         sideBarMenus.forEach((e) => {
             e.classList.remove("hidden");
         })
     } else {
         sideBar?.classList.replace("translate-x-0", "-translate-x-full");
-        sideBar?.classList.replace("md:max-w-48", "md:max-w-20");
+        sideBar?.classList.replace("min-w-80", "min-w-16");
 
         sideBarMenus.forEach((e) => {
             e.classList.add("hidden");
--- resources/views/components/dashboard-side-bar.blade.php
+++ resources/views/components/dashboard-side-bar.blade.php
@@ -1,38 +1,41 @@
-<aside id="sideBar" class="fixed md:relative top-0 h-screen bg-gray-800 
-text-white -translate-x-full md:translate-x-0 transition-all 
-duration-300 ease-in-out z-10 py-4 px-6 w-full sm:w-64 md:max-w-20">
+<aside id="sideBar" class="fixed md:relative top-0 h-screen bg-gray-800 text-white -translate-x-full md:translate-x-0 transition-all duration-300 ease-in-out z-10 py-4 px-6 min-w-16">
+  
+  <!-- This is just here just cause of tailwind not registering classes -->
+  <div class="min-w-80 hidden"></div>
 
   <div class="flex flex-col gap-4 justify-center">
-    <div class="flex gap-3 items-center justify-between">
+    <div class="flex gap-3 items-center justify-between p-2">
       <div class="flex gap-2 items-center">
         <img src="..." alt="Not available" class="rounded-full size-12 md:hidden">
         <i class="fa-solid fa-arrow-right-from-bracket text-xl"></i>
         <a href="{{ route('logout') }}" class="menu-text text-red-600 hidden">Logout</a>
       </div>
 
-      <button class="sideBarToggle outline-none text-xl active:scale-90 delay-100 transition-all hover:scale-110 hover:text-gray-500 md:hidden">
+      <button class="sideBarToggle outline-none text-xl active:scale-90 delay-100 transition-all hover:scale-105 hover:text-gray-500 md:hidden">
         <i class="fa-solid fa-x"></i>
       </button>
     </div>
 
-    <div class="flex flex-col gap-3">
-      <a href="{{ route('users.index') }}" class="flex gap-2 hover:scale-110 hover:text-gray-400 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
-        <i class="fa-solid fa-chart-line text-xl"></i>
+    <hr>
+
+    <div class="flex flex-col gap-4 justify-center">
+      <a href="{{ route('users.index') }}" class="flex items-center gap-2 rounded-md hover:scale-105 p-2 hover:text-gray-400 hover:bg-slate-300 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
+        <i class="fa-solid fa-user text-xl"></i>
         <span class="menu-text hidden">Users</span>
       </a>
-      <a href="{{ route('forums.index') }}" class="flex gap-2 hover:scale-110 hover:text-gray-400 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
+      <a href="{{ route('forums.index') }}" class="flex items-center gap-2 rounded-md hover:scale-105 p-2 hover:text-gray-400 hover:bg-slate-300 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
         <i class="fa-solid fa-newspaper text-xl"></i>
         <span class="menu-text hidden">Forums</span>
       </a>
-      <a href="{{ route('posts.index') }}" class="flex gap-2 hover:scale-110 hover:text-gray-400 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
+      <a href="{{ route('posts.index') }}" class="flex items-center gap-2 rounded-md hover:scale-105 p-2 hover:text-gray-400 hover:bg-slate-300 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
         <i class="fa-solid fa-scroll text-xl"></i>
         <span class="menu-text hidden">Posts</span>
       </a>
-      <a href="{{ route('comments.index') }}" class="flex gap-2 hover:scale-110 hover:text-gray-400 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
+      <a href="{{ route('comments.index') }}" class="flex items-center gap-2 rounded-md hover:scale-105 p-2 hover:text-gray-400 hover:bg-slate-300 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
         <i class="fa-solid fa-comment text-xl"></i>
         <span class="menu-text hidden">Comment</span>
       </a>
-      <a href="{{ route('reply.index') }}" class="flex gap-2 hover:scale-110 hover:text-gray-400 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
+      <a href="{{ route('reply.index') }}" class="flex items-center gap-2 rounded-md hover:scale-105 p-2 hover:text-gray-400 hover:bg-slate-300 active:scale-90 active:text-gray-800 duration-200 ease-in-out transition-all">
         <i class="fa-solid fa-comments text-xl"></i>
         <span class="menu-text hidden">Replies</span>
       </a>
