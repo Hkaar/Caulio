7c485ed58e106801e749d87a5ce6ba736955621c
Hkaar <HkaarDev@gmail.com>
2024-05-13 20:32:02 +0800

    Updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,31 +33,32 @@
   </div>
 </section>
 
-<section id="promoMsg1" class="bg-slate-300 py-10">
+<section id="promoMsg1" class="bg-slate-300 py-20">
   <div class="container flex justify-between items-center flex-col md:flex-row h-full gap-10">
     <article class="space-y-3 flex-1 text-center md:text-start">
       <h3 class="text-3xl font-bold">
         Community made forums
       </h3>
 
-      <p>
-        Forums made by the community for your interests
+      <p class="max-w-[80%]">
+        Join vibrant communities tailored to your interests. From technology to art, there's a forum 
+        for everyone. Dive into discussions curated by passionate individuals who share your enthusiasm.
       </p>
     </article>
 
     <div class="mockup-window border bg-gray-700 flex-1 w-full">
       <div class="flex flex-col items-center px-4 py-16 bg-white h-full">
-        Forum
+        <img src="{{ URL::asset("assets/imgs/forum.png") }}" alt="Not available" srcset="" class="max-w-full max-h-80 object-cover">
       </div>
     </div>
   </div>
 </section>
 
-<section id="promoMsg2" class="py-10 bg-slate-800 text-white">
+<section id="promoMsg2" class="py-20 bg-slate-800 text-white">
   <div class="container flex justify-between items-center flex-col-reverse md:flex-row h-full gap-10">
     <div class="mockup-window border bg-gray-700 flex-1 w-full">
       <div class="flex flex-col items-center px-4 py-16 bg-white h-full">
-        Forum
+        <img src="{{ URL::asset("assets/imgs/topics.png") }}" alt="Not available" srcset="" class="max-w-full max-h-80 object-cover">
       </div>
     </div>
 
@@ -66,8 +67,10 @@
         Various topics
       </h3>
 
-      <p>
-        Various topics for you to talk & discuss about
+      <p class="max-w-[80%]">
+        Explore a myriad of topics spanning from the latest trends to timeless classics. Whether 
+        you're seeking advice, sharing experiences, or simply engaging in lively debates, Caulio 
+        offers a diverse range of discussions to suit your curiosity.
       </p>
     </article>
   </div>
@@ -125,62 +128,5 @@
  </div>
 </section>
 
-<footer class="bg-slate-800 text-white">
-  <div class="container flex justify-between flex-col md:flex-row h-full gap-3 py-10">
-    <div class="space-y-3 flex-1">
-      <h3 class="text-3xl font-bold">
-        Caulio
-      </h3>
-
-      <div class="flex flex-col gap-1">
-        <a href="#">About Us</a>
-        <a href="#">Contact Us</a>
-      </div>
-    </div>
-
-    <div class="space-y-3 flex-1">
-      <h3 class="text-3xl font-bold">
-        Contact us!
-      </h3>
-
-      <div class="flex gap-3 text-2xl">
-        <a href="#">
-          <i class="fa-brands fa-youtube"></i>
-        </a>
-      
-        <a href="#">
-          <i class="fa-brands fa-instagram"></i>
-        </a>
-
-        <a href="#">
-          <i class="fa-brands fa-twitter"></i>
-        </a>
-      </div>
-    </div>
-
-    <div class="space-y-3 flex-1 hidden md:block">
-      <h3 class="text-3xl font-bold">
-        Sign up for the newsletter
-      </h3>
-
-      <form class="flex">
-        <input type="email" placeholder="Enter your email here" class="input input-bordered w-full max-w-xs text-black" />
-        <button type="submit" class="btn btn-primary">Signup</button>
-      </form>
-    </div>
-  </div>
-
-  <div class="w-full bg-slate-700 py-3">
-    <div class="container flex justify-between items-center flex-col md:flex-row gap-3">
-      <span class="font-bold text-center md:text-start">
-        &#169; Copyright 2024 Caulio Team. All rights reserved.
-      </span>
-
-      <div class="flex gap-3 text-center md:text-start">
-        <a href="#">Terms of service</a>
-        <a href="#">Privacy Policy</a>
-      </div>
-    </div>
-  </div>
-</footer>
+<x-footer></x-footer>
 @endsection
